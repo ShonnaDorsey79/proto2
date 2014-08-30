23b162ef0d4077bd30cd4c481d90b07b5e43660a
shonna <shonna@interfaceschool.com>
2014-08-30 14:46:35 +0000

    database updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :links do
     resources :comments
   end
-  root :to => redirect('/links')
+  root "links#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140819021620) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.string   "author"
     t.text     "body"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140819021620) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["link_id"], name: "index_comments_on_link_id"
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id", using: :btree
 
   create_table "links", force: true do |t|
     t.string   "title"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20140819021620) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
